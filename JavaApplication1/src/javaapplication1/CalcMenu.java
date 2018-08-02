4a789130231b9bc78c4287514b665042c3c111df
rougan <cristian.pineros@endava.com>
2018-08-02 12:07:33 -0500

    Test suma completado

--- JavaApplication1/src/javaapplication1/CalcMenu.java
+++ JavaApplication1/src/javaapplication1/CalcMenu.java
@@ -1,4 +1,7 @@
+package javaapplication1;
+
 import java.util.Scanner;
+import javaapplication1.Calc;
 
 
 /**
@@ -6,14 +9,22 @@ import java.util.Scanner;
    Optional: If you want, you can create separate classes in order to wide 
    the methods cohesion concept, to the SRP-SOLID principle at class level.
 */
-public class Calc {
-    
+public class CalcMenu {
+    public static double doOperation(int number1,int number2,String symbol){
+            if(symbol.equals("+")==true){
+                return number1+number2;
+            }
+            
+            return -1;
+        }
     public static void main(String[] args) {
         double n1, n2;
         String operation;
-        Scanner scannerObject = new Scanner(System.in);
-
-        System.out.println("Enter first number");
+        //Scanner scannerObject = new Scanner(System.in);
+        Calc calculator=new Calc();
+        
+        
+        /*System.out.println("Enter first number");
         n1 = scannerObject. nextDouble();
 
         System.out.println("Enter second number");
@@ -43,6 +54,6 @@ public class Calc {
         default:
             System.out.println("Unsupported Operation");
 
-        }
+        }*/
     }
 }
---
+++ JavaApplication1/test/TDD/CalcTest.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TDD;
+
+import javaapplication1.CalcMenu;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author cpineros
+ */
+public class CalcTest {
+    
+    @Test
+    public void TestSuma(){
+        assertEquals(7,CalcMenu.doOperation(2,5,"+"),0);
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
